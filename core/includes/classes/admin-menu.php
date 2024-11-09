ba4a8309bbc56c9dad365ad3e3f335729ac43150
Christopher <cad@thecije.org>
2024-11-08 23:25:01 -0500

    Update admin-menu.php

--- core/includes/classes/admin-menu.php
+++ core/includes/classes/admin-menu.php
@@ -4,21 +4,30 @@ add_action('admin_menu', 'weather_graph_plugin_menu');
 
 function weather_graph_plugin_menu() {
     add_options_page(
-        'Weather Graph Plugin Settings', // Page title
-        'Weather Graph',                 // Menu title
+        'Weather Hub Plugin Settings', // Page title
+        'Weather Hub',                 // Menu title
         'manage_options',                // Capability
-        'weather-graph-plugin',          // Menu slug
-        'weather_graph_plugin_settings_page' // Callback function
+        'weather-hub-plugin',          // Menu slug
+        'weather_hub_plugin_settings_page' // Callback function
     );
 }
 
 function weather_graph_plugin_settings_page() {
     ?>
     <div class="wrap">
-        <h1>Weather Graph Plugin</h1>
+        <h1>Weather Graph </h1>
         <p>To display the weather graph, use the following shortcode:</p>
         <pre><code>[weather_graph]</code></pre>
         <p>Insert this shortcode into any post or page where you want the graph to appear.</p>
+        <h1>Weather Map </h1>
+        <p>To display the weather map, use the following shortcode:</p>
+        <pre><code>[weather_map]</code></pre>
+        <p>Insert this shortcode into any post or page where you want the weather data map to appear.</p>
+        <h1>Weather Station Registration </h1>
+        <p>To display the weather station registration form, use the following shortcode:</p>
+        <pre><code>[register_station]</code></pre>
+        <p>Insert this shortcode into any post or page where you want the weather station registration form to appear.</p>
     </div>
     <?php
 }
+?>
